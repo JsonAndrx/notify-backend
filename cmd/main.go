e8cbb06c011732451a7ce44aea9fe3fc36c2c9b1
JsonAndrx <yeison.pev1@gmail.com>
2024-12-21 11:48:08 -0500

    adding log to debug deploy

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	response "notify-backend/api/utils/response"
 	"notify-backend/api/routes"
+	response "notify-backend/api/utils/response"
+
+	"notify-backend/api/utils/debug"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/rs/zerolog/log"
 	"github.com/joho/godotenv"
-	"notify-backend/api/utils/debug"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -20,7 +22,7 @@ func main() {
 
 	log.Info().Msg("Starting server")
 	r := gin.Default()
-	
+
 	r.Use(cors.New(cors.Config{
 		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -28,6 +30,7 @@ func main() {
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
+	fmt.Println("paso los cors")
 	routes.SeptupRoutes(r)
 
 	server := &http.Server{
